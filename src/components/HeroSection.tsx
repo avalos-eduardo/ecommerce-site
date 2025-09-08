9033c33c8a78eef0c6285c7fb751906994291179
avalos-eduardo <178445917+avalos-eduardo@users.noreply.github.com>
2025-09-07 20:28:00 -0500

    implemented Home and HeroSection components
    Home page will hold the HeroSection component and the list of available products

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,34 @@
+interface HeroSectionProps {
+  header: string;
+  paragraph: string;
+  imgSrc: string;
+}
+
+export default function HeroSection({
+  header,
+  paragraph,
+  imgSrc,
+}: HeroSectionProps) {
+  return (
+    <section className="pt-5 flex flex-col md:pt-15 md:flex-row h-auto">
+      <div className="flex px-10 flex-col items-center justify-center text-center md:w-[50%] [&>*]:my-2">
+        <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold mb-4 text-[#6c9469]">
+          {header}
+        </h1>
+        <p className="text-2xl sm:text-3xl pb-1 max-w-md ">{paragraph}</p>
+        <button className="bg-[#6c9469] px-15 py-3 rounded-3xl text-white cursor-pointer">
+          Shop Now
+        </button>
+      </div>
+      <div className="p-5 flex justify-center items-center md:w-[50%]">
+        <div className="w-75 pt-5 h-auto md:w-100 lg:w-110 bg-transparent rounded-xl flex items-center justify-center font-semibold">
+          <img
+            src={imgSrc}
+            alt="image of headphones"
+            className="object-fill rounded-xl shadow-2xl"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import HeroSection from "./HeroSection";
+
+export default function Home() {
+  return (
+    <main>
+      <HeroSection
+        header="All your interersts in one place."
+        paragraph="Discover a wide selection of products tailored just for you. Enjoy seamless shopping and exclusive deals every day."
+        imgSrc="https://cdn.thewirecutter.com/wp-content/media/2023/07/bluetoothheadphones-2048px-0876.jpg"
+      />
+    </main>
+  );
+}
