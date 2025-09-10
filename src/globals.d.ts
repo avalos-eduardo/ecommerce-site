fb224a0c7cc7f35d8c7d4b3a731f08bd557de722
avalos-eduardo <178445917+avalos-eduardo@users.noreply.github.com>
2025-09-09 23:39:02 -0500

    installed react-router-hash-link

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,3 +1,20 @@
 declare module "*.css";
 declare module "@fontsource/*" {}
 declare module "@fontsource-variable/*" {}
+
+declare module "react-router-hash-link" {
+  import * as React from "react";
+  import { LinkProps } from "react-router-dom";
+
+  export interface HashLinkProps extends LinkProps {
+    smooth?: boolean;
+    scroll?: (el: HTMLElement) => void;
+  }
+
+  export const HashLink: React.ForwardRefExoticComponent<
+    HashLinkProps & React.RefAttributes<HTMLAnchorElement>
+  >;
+  export const NavHashLink: React.ForwardRefExoticComponent<
+    HashLinkProps & React.RefAttributes<HTMLAnchorElement>
+  >;
+}
