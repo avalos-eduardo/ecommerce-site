e77b44de0ee42366410f0c55da807510b177310b
avalos-eduardo <178445917+avalos-eduardo@users.noreply.github.com>
2025-09-11 22:04:43 -0500

    implemented search, filter, and sort functions

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -7,6 +7,10 @@ export default function Products() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
+  const [searchInput, setSearchInput] = useState("");
+  const [categoryFilter, setCategoryFilter] = useState("all");
+  const [sortOption, setSortOption] = useState("default");
+
   useEffect(() => {
     const getProducts = async () => {
       try {
@@ -26,16 +30,68 @@ export default function Products() {
     getProducts();
   }, []);
 
+  const filteredProducts: Product[] = products
+    .filter((p) => p.title.toLowerCase().includes(searchInput.toLowerCase()))
+    .filter((p) => categoryFilter === "all" || p.category === categoryFilter)
+    .sort((a, b) => {
+      switch (sortOption) {
+        case "name":
+          return a.title.localeCompare(b.title);
+        case "priceLowHigh":
+          return a.price - b.price;
+        case "priceHighLow":
+          return b.price - a.price;
+        default:
+          return 0;
+      }
+    });
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
 
   return (
-    <section id="products" className="px-10 pt-15 pb-20">
-      <h1 className="text-lg">Items: ({products.length})</h1>
+    <section id="products" className="flex flex-col px-10 pt-15 pb-20">
+      <input
+        placeholder="Search Products"
+        id="search-input"
+        type="text"
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
+        className="bg-gray-300 rounded-lg px-2 h-8 placeholder:text-center text-center mb-5 lg:h-10"
+      />
+
+      <div className="flex justify-between pb-5">
+        <select
+          className="rounded-lg w-[49%] text-center h-7 md:h-9 bg-gray-700 text-white cursor-pointer"
+          value={categoryFilter}
+          onChange={(e) => setCategoryFilter(e.target.value)}
+        >
+          <option value="all">All Categories</option>
+          <option value="men's clothing">Men's Clothing</option>
+          <option value="women's clothing">Women's Clothing</option>
+          <option value="jewelery">Jewelry</option>
+          <option value="electronics">Electronics</option>
+        </select>
+
+        <select
+          className="rounded-lg w-[49%] text-center h-7 md:h-9 bg-gray-700 text-white cursor-pointer"
+          value={sortOption}
+          onChange={(e) => setSortOption(e.target.value)}
+        >
+          <option value="default">Sort</option>
+          <option value="name">Sort by Name</option>
+          <option value="priceLowHigh">Sort by Price (Low to High)</option>
+          <option value="priceHighLow">Sort by Price (High to Low)</option>
+        </select>
+      </div>
+
+      <h1 className="text-lg">Items: ({filteredProducts.length})</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5 text-center pt-2">
-        {products.map((p) => (
-          <ProductCard key={p.id} product={p} />
-        ))}
+        {filteredProducts.length > 0 ? (
+          filteredProducts.map((p) => <ProductCard key={p.id} product={p} />)
+        ) : (
+          <p>No results found.</p>
+        )}
       </div>
     </section>
   );
