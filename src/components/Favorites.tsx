3daf747591c395cd6a1e450e5579077af1f47b1c
avalos-eduardo <178445917+avalos-eduardo@users.noreply.github.com>
2025-09-24 22:56:16 -0500

    implemented page where users can see items they have favorited

--- src/components/Favorites.tsx
+++ src/components/Favorites.tsx
@@ -1,7 +1,21 @@
+import { useShopContext } from "../context/shopContext";
+import ProductCard from "./ProductCard";
+
 export default function Favorites() {
+  const { state } = useShopContext();
+
   return (
     <main>
-      <p>hmmm</p>
+      <h1 className="pt-5 px-10 open-sauce-one-font text-2xl md:text-3xl">
+        Your Favorites
+      </h1>
+      <section className="flex flex-col px-10 pt-15 pb-20">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 text-center pt-2">
+          {state.favorites.map((p) => (
+            <ProductCard key={p.id} product={p} />
+          ))}
+        </div>
+      </section>
     </main>
   );
 }
